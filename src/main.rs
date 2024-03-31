f160e24499f938ccb93ba1f23e91d845ad6886d4
Josh Clements <clements604@gmail.com>
2024-03-31 15:47:20 +1100

    Keyboard working

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,6 @@ mod cpu;
 mod constants;
 mod display;
 
-/*fn main() {
-    let cpu: cpu::Cpu = cpu::Cpu::new();
-    println!("Hello, world!");
-    let platform: platform::Platform = platform::Platform::new("Hello".to_string(), 800, 600, 64, 32);
-    // sleep for 60 seconds
-    std::thread::sleep(std::time::Duration::from_secs(60));
-}*/
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
@@ -37,10 +29,10 @@ fn main() {
     let mut event_pump = display.sdl_context.event_pump().unwrap();
 
     while ! quit {
-        //quit = platform.process_input(&mut cpu.keypad);
+
+        //std::thread::sleep(Duration::from_millis(100));
 
         for event in event_pump.poll_iter() {
-            println!("{:?}", event);
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
@@ -50,6 +42,35 @@ fn main() {
                     quit = true;
                     break;
                 },
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    ..
+                } => {
+                    println!("{:?}", keycode);
+                    // Reset keyboard
+                    for i in 0..16 {
+                        cpu.keyboard[i] = 0;
+                    }
+                    match keycode {
+                        Keycode::Num1 => cpu.keyboard[0x1] = 1,
+                        Keycode::Num2 => cpu.keyboard[0x2] = 1,
+                        Keycode::Num3 => cpu.keyboard[0x3] = 1,
+                        Keycode::Num4 => cpu.keyboard[0xC] = 1,
+                        Keycode::Q => cpu.keyboard[0x4] = 1,
+                        Keycode::W => cpu.keyboard[0x5] = 1,
+                        Keycode::E => cpu.keyboard[0x6] = 1,
+                        Keycode::R => cpu.keyboard[0xD] = 1,
+                        Keycode::A => cpu.keyboard[0x7] = 1,
+                        Keycode::S => cpu.keyboard[0x8] = 1,
+                        Keycode::D => cpu.keyboard[0x9] = 1,
+                        Keycode::F => cpu.keyboard[0xE] = 1,
+                        Keycode::Z => cpu.keyboard[0xA] = 1,
+                        Keycode::X => cpu.keyboard[0x0] = 1,
+                        Keycode::C => cpu.keyboard[0xB] = 1,
+                        Keycode::V => cpu.keyboard[0xF] = 1,
+                        _ => {}
+                    }
+                },
                 _ => println!("Unhandled event"),
             }
         }
@@ -58,6 +79,8 @@ fn main() {
             display.redraw(&cpu.display);
             cpu.draw_flag = false;
         }
+
+        println!("CPU {}", cpu);
 
         cpu.cycle();
 
@@ -70,7 +93,9 @@ fn main() {
             }
             cycle_time = Instant::now();
         }
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 2564));
+        //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 2564));
+
+        ::std::thread::sleep(Duration::from_micros(1000)); // Sleep for 1 millisecond
     }
 
 }
